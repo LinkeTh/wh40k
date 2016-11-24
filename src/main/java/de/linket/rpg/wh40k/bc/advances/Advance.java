ba2719ebaf12a344815d5d118107094365b077d4
Thomas Linke <t.linke@pripares.com>
2016-11-24 10:14:37 +0100

    fixed radio buttons for skill selection

--- src/main/java/de/linket/rpg/wh40k/bc/advances/Advance.java
+++ src/main/java/de/linket/rpg/wh40k/bc/advances/Advance.java
@@ -3,9 +3,10 @@ package de.linket.rpg.wh40k.bc.advances;
 import de.linket.rpg.wh40k.bc.common.Modifier;
 import de.linket.rpg.wh40k.bc.exp.Experience;
 
-public interface Advance<T extends Modifier<?>> {
+public interface Advance<T extends Modifier<?>>
+{
 
-	public T getModifier();
+    public T getModifier();
 
-	public Experience getCosts();
+    public Experience getCosts();
 }
